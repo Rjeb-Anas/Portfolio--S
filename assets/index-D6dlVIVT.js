5e8d88398cb496b6aee146199601f2a64879813c
Anass <anassrjeb929@gmail.com>
2026-07-17 13:53:07 +0100

    Updates

--- assets/index-D6dlVIVT.js
+++ assets/index-D6dlVIVT.js
@@ -32,24 +32,23 @@ Error generating stack: `+e.message+`
                         flex-col w-30 h-auto rounded-lg bg-cyan-950 shadow-lg shadow-black
                          py-2
                         text-[clamp(1rem,2vw,1.25rem)]
-                        ${n?`flex animate-menu-open`:`hidden `}`,children:[(0,b.jsx)(`a`,{href:``,className:`w-full text-center py-2 hover:bg-white/10`,children:`Home`}),(0,b.jsx)(`a`,{href:``,className:`w-full text-center py-2 hover:bg-white/10`,children:`About`}),(0,b.jsx)(`a`,{href:``,className:`w-full text-center py-2 hover:bg-white/10`,children:`Contact`})]})]})]})})},ie=()=>(0,b.jsx)(`main`,{className:`mx-auto min-h-[calc(100dvh-6.25rem)]  w-full\r
+                        ${n?`flex animate-menu-open`:`hidden `}`,children:[(0,b.jsx)(`a`,{href:``,className:`w-full text-center py-2 hover:bg-white/10`,children:`Home`}),(0,b.jsx)(`a`,{href:``,className:`w-full text-center py-2 hover:bg-white/10`,children:`About`}),(0,b.jsx)(`a`,{href:``,className:`w-full text-center py-2 hover:bg-white/10`,children:`Contact`})]})]})]})})},ie=`/Portfolio--S/assets/portfolio-BpyCJYcP.png`,ae=()=>(0,b.jsx)(`main`,{className:`mx-auto min-h-[calc(100dvh-6.25rem)]  w-full\r
     max-w-7xl px-4 sm:px-6 lg:px-8`,children:(0,b.jsxs)(`section`,{className:`flex md:flex-row max-md:flex-col\r
              max-md:items-center w-full  gap-6  py-10 relative h-150 `,children:[(0,b.jsxs)(`div`,{className:`md:w-1/2 max-md:h-1/2 h-auto flex flex-col\r
                  justify-center`,children:[(0,b.jsx)(`h1`,{className:`text-[clamp(2rem,5vw,3rem)] py-2 w-full text-center font-bold`,children:`Anass Rjeb`}),(0,b.jsx)(`h2`,{className:`text-[clamp(1rem,2.5vw,1.5rem)] py-2 w-full text-center`,children:`Front-End Developer`}),(0,b.jsx)(`p`,{className:`text-[clamp(0.9rem,1.5vw,1rem)] py-2 w-full text-center leading-relaxed`,children:`Computer Science student and Front-End Developer passionate about creating responsive, modern, and accessible web applications using React, Tailwind CSS, and JavaScript.`}),(0,b.jsx)(`div`,{className:`w-full flex py-3 \r
                     items-center justify-center`,children:(0,b.jsx)(`a`,{href:``,className:`py-3 px-10\r
-                        max-md:py-1 bg-red-500 rounded-full\r
+                        max-md:py-2 bg-red-500 rounded-full\r
                         hover:bg-red-400`,children:`Resume`})})]}),(0,b.jsx)(`div`,{className:`md:w-1/2 max-md:h-1/2 flex items-center justify-center `,children:(0,b.jsx)(`div`,{className:`md:w-[30vw] max-md:w-[60vw] h-40 \r
                     md:h-60 \r
-                    border \r
-                    rounded-xl`})}),(0,b.jsx)(`a`,{href:`#Cursus`,className:`w-auto h-auto   border rounded-full absolute bottom-0  left-1/2 -translate-x-1/2`,children:(0,b.jsx)(`button`,{class:`cursor-pointer relative after:content-['scroll_to_bottom']\r
+                    rounded-xl`,children:(0,b.jsx)(`img`,{src:ie,alt:`Portfolio`,className:`size-full rounded-xl`})})}),(0,b.jsx)(`a`,{href:`#Cursus`,className:`w-auto h-auto   border rounded-full absolute bottom-0  left-1/2 -translate-x-1/2`,children:(0,b.jsx)(`button`,{class:`cursor-pointer relative after:content-['scroll_to_bottom']\r
                          after:text-white after:absolute after:text-nowrap after:scale-0 \r
                          hover:after:scale-100 after:duration-200 w-10 h-10 rounded-full  border-4 border-sky-200 bg-black pointer \r
                          flex items-center justify-center duration-300 hover:rounded-[50px] hover:w-36 group/button overflow-hidden active:scale-90`,children:(0,b.jsx)(`svg`,{class:`w-2.5 fill-white delay-50 duration-200 group-hover/button:-translate-y-12 rotate-180`,viewBox:`0 0 384 512`,children:(0,b.jsx)(`path`,{d:`M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 \r
-                                45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z`})})})})]})}),ae=()=>(0,b.jsx)(b.Fragment,{children:(0,b.jsx)(`main`,{id:`Cursus`,className:`mx-auto min-h-dvh  w-full \r
+                                45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z`})})})})]})}),oe=()=>(0,b.jsx)(b.Fragment,{children:(0,b.jsx)(`main`,{id:`Cursus`,className:`mx-auto min-h-dvh  w-full \r
     max-w-7xl px-4 sm:px-6 lg:px-8`,children:(0,b.jsx)(`section`,{className:`flex flex-col items-center gap-6 py-12 md:flex-row \r
-            justify-center`})})});function oe(){return(0,b.jsx)(b.Fragment,{children:(0,b.jsx)(`main`,{className:`min-h-dvh w-full overflow-x-hidden text-black dark:text-white
+            justify-center`})})});function se(){return(0,b.jsx)(b.Fragment,{children:(0,b.jsx)(`main`,{className:`min-h-dvh w-full overflow-x-hidden text-black dark:text-white
       from-gray-50 to-gray-100
       dark:from-gray-800 dark:to-gray-900
       bg-linear-to-br  animate-load-fast `,children:(0,b.jsxs)(`div`,{className:`mx-auto flex min-h-dvh w-full max-w-7xl flex-col md:border-l-cyan-950 
        md:border-r-cyan-950 
-        md:border-l-4 md:border-r-4`,children:[(0,b.jsx)(w,{}),(0,b.jsx)(ie,{}),(0,b.jsx)(ae,{})]})})})}(0,v.createRoot)(document.getElementById(`root`)).render((0,b.jsx)(_.StrictMode,{children:(0,b.jsx)(oe,{})}));
+        md:border-l-4 md:border-r-4`,children:[(0,b.jsx)(w,{}),(0,b.jsx)(ae,{}),(0,b.jsx)(oe,{})]})})})}(0,v.createRoot)(document.getElementById(`root`)).render((0,b.jsx)(_.StrictMode,{children:(0,b.jsx)(se,{})}));
